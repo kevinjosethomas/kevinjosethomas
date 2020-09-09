c6731e2ed428394600347d4dffeda0507c42f613
TrustedMercury <kevin.jt2007@gmail.com>
2020-09-09 13:25:55 -0400

    Added / route source code

---
+++ src/routes/home.js
@@ -0,0 +1,10 @@
+import path from "path";
+import express from "express";
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/views/home.html'"))
+})
+
+export default router;
