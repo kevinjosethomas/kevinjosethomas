c27cd7b5cece47c65fb32b5e18113f3990ad1477
TrustedMercury <kevin.jt2007@gmail.com>
2020-09-09 13:47:53 -0400

    Minor Bug Fixes

--- src/routes/home.js
+++ src/routes/home.js
@@ -4,7 +4,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/views/home.html'"))
+  res.sendFile(path.join(__dirname, "/public/views/home.html"))
 })
 
 export default router;
