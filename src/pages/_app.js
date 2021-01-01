d41781220362e23d18f077393ad66f2b0332a691
TrustedMercury <kevin.jt2007@gmail.com>
2021-01-01 12:59:41 -0500

    Added basic _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,32 @@
+import Head from "next/head";
+import { Fragment } from "react";
+
 import "../assets/styles/tailwind.css";
 
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Fragment>
+      <Head>
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://pro.fontawesome.com/releases/v5.15.1/css/all.css"
+        />
+        <link
+          rel="stylesheet"
+          href="https://use.typekit.net/ebw0chl.css"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </Fragment>
+  )
 }
 
 export default App;
