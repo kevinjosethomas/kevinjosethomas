65e6f6287cecf24f8f231e0784faa41647d986a1
TrustedMercury <kevin.jt2007@gmail.com>
2021-01-01 13:17:57 -0500

    Added temporary home page

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import { Fragment } from "react";
+
+import { BasicLayout } from "../layouts/Basic";
+
+const Index = (props) => {
+
+  return (
+    <Fragment>
+      <BasicLayout>
+        <h1>hello</h1>
+      </BasicLayout>
+    </Fragment>
+  )
+
+}
+
+export default Index;
