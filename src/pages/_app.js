06b0016bf2b35d1af8ac21a44bdafd59b2b63141
TrustedMercury <kevin.jt2007@gmail.com>
2021-02-11 19:30:41 -0500

    Added TailwindCSS and PostCSS

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,4 @@
+import "../styles/tailwind.css";
 
 function App({Component, pageProps}) {
   return <Component {...pageProps} />;
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: ["./src/**/*.js"],
+  theme: {
+    extend: {
+      colors: {
+        dark: {
+          100: "#161820",
+          200: "#1C1D28",
+          300: "#202230",
+          400: "#282C3B"
+        }
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
