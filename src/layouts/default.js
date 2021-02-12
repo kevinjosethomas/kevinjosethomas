008be1055ea57427ae7de8da25ce03d63d553a6f
TrustedMercury <kevin.jt2007@gmail.com>
2021-02-12 10:29:38 -0500

    Made width constant

--- src/layouts/default.js
+++ src/layouts/default.js
@@ -3,8 +3,8 @@ import { Navbar } from "../components/Navbar";
 export const Default = (props) => {
 
   return (
-    <div className="bg flex flex-col items-start justify-center h-full min-h-screen">
-      <div className="flex flex-col items-start justify-center h-screen">
+    <div className="bg flex flex-col items-start justify-center w-full h-full min-h-screen">
+      <div className="flex flex-col items-start justify-center w-full h-screen">
         <Navbar />
         { props.children }
       </div>
