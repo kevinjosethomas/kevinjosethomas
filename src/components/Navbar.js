ec2862f0a5fdbf12f55c4292e10474a9b565118a
TrustedMercury <kevin.jt2007@gmail.com>
2021-02-11 22:51:07 -0500

    Changed home icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ export const Navbar = () => {
   return (
     <nav className="flex flex-row items-center justify-start w-full px-40 pt-10">
       <Link href="/">
-        <a className="mr-4 font-medium text-2xl text-gray-200 hover:text-gray-400"><i className="far fa-home" /></a>
+        <a className="mr-4 font-medium text-2xl text-gray-200 hover:text-gray-400"><i className="fal fa-home" /></a>
       </Link>
       <Link href="/projects">
         <a className="mx-4 font-inter font-medium text-2xl text-gray-200 hover:text-gray-400">projects</a>
