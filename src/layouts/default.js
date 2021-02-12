8b5ebd0fa79f37be23eecb7d513afbea9cf2775e
TrustedMercury <kevin.jt2007@gmail.com>
2021-02-11 22:30:00 -0500

    Updated Default layout for only one image

--- src/layouts/default.js
+++ src/layouts/default.js
@@ -2,10 +2,8 @@
 export const Default = (props) => {
 
   return (
-    <div className="bg flex flex-col items-center justify-center w-screen max-w-full h-full min-h-screen">
-      <div className="flex flex-col items-center justify-center w-full h-full bg-dark-100 bg-opacity-50">
-        { props.children }
-      </div>
+    <div className="bg flex flex-col items-start justify-center h-full min-h-screen ">
+      { props.children }
     </div>
   )
 
