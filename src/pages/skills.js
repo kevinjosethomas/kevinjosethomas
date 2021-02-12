6824e3652ff3b157ea661e8869fa31de979b3c4e
TrustedMercury <kevin.jt2007@gmail.com>
2021-02-12 13:51:54 -0500

    Added skills page

---
+++ src/pages/skills.js
@@ -0,0 +1,149 @@
+import { Default } from "../layouts/default";
+
+const Skills = (props) => {
+
+  return (
+    <Default>
+      <div className="flex flex-col items-start justify-center my-20 px-40 2xl:px-52 w-full">
+        <div className="flex flex-col items-start justify-center mb-4">
+          <span className="mb-2 font-proxima font-bold text-3xl text-gray-300">CONCEPTS</span>
+          <div className="flex flex-row items-center justify-start flex-wrap mt-2">
+            <div className="flex flex-row items-center justify-center mx-2 py-2 px-4 bg-black bg-opacity-40 rounded">
+              <i className="fas fa-columns mr-1 text-xl text-gray-100" />
+              <span className="ml-1 font-proxima font-semibold text-xl text-gray-300">FRONTEND WEB</span>
+            </div>
+            <div className="flex flex-row items-center justify-center mx-2 my-2 py-2 px-4 bg-black bg-opacity-40 rounded">
+              <i className="fas fa-router mr-1 text-xl text-gray-100" />
+              <span className="ml-1 font-proxima font-semibold text-xl text-gray-300">BACKEND APIs</span>
+            </div>
+            <div className="flex flex-row items-center justify-center mx-2 my-2 py-2 px-4 bg-black bg-opacity-40 rounded">
+              <i className="fas fa-database mr-1 text-xl text-gray-100" />
+              <span className="ml-1 font-proxima font-semibold text-xl text-gray-300">DATABASES</span>
+            </div>
+            <div className="flex flex-row items-center justify-center mx-2 my-2 py-2 px-4 bg-black bg-opacity-40 rounded">
+              <i className="fas fa-spider mr-1 text-xl text-gray-100" />
+              <span className="ml-1 font-proxima font-semibold text-xl text-gray-300">CRAWLERS</span>
+            </div>
+            <div className="flex flex-row items-center justify-center mx-2 py-2 px-4 bg-black bg-opacity-40 rounded">
+              <i className="far fa-pencil-paintbrush mr-1 text-xl text-gray-100" />
+              <span className="ml-1 font-proxima font-semibold text-xl text-gray-300">WEB DESIGN</span>
+            </div>
+            <div className="flex flex-row items-center justify-center mx-2 my-2 py-2 px-4 bg-black bg-opacity-40 rounded">
+              <i className="fab fa-discord mr-1 text-xl text-gray-100" />
+              <span className="ml-1 font-proxima font-semibold text-xl text-gray-300">DISCORD BOTS</span>
+            </div>
+            <div className="flex flex-row items-center justify-center mx-2 my-2 py-2 px-4 bg-black bg-opacity-40 rounded">
+              <i className="fas fa-box-open mr-1 text-xl text-gray-100" />
+              <span className="ml-1 font-proxima font-semibold text-xl text-gray-300">PY PACKAGES</span>
+            </div>
+            <div className="flex flex-row items-center justify-center mx-2 my-2 py-2 px-4 bg-black bg-opacity-40 rounded">
+              <i className="fas fa-users mr-1 text-xl text-gray-100" />
+              <span className="ml-1 font-proxima font-semibold text-xl text-gray-300">COMMUNITY MANAGEMENT</span>
+            </div>
+            <div className="flex flex-row items-center justify-center mx-2 my-2 py-2 px-4 bg-black bg-opacity-40 rounded">
+              <i className="far fa-dollar-sign mr-1 text-xl text-gray-100" />
+              <span className="ml-1 font-proxima font-semibold text-xl text-gray-300">FREELANCING</span>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col items-start justify-center my-4">
+          <span className="mb-2 font-proxima font-bold text-3xl text-gray-300">SOFTWARE</span>
+          <div className="flex flex-row items-center justify-start flex-wrap mt-2">
+            <div className="flex flex-row items-center justify-center mx-2 py-2 px-4 bg-black bg-opacity-40 rounded">
+              <i className="fab fa-python mr-1 text-xl text-gray-100" />
+              <span className="ml-1 font-proxima font-semibold text-xl text-gray-300">PYTHON</span>
+            </div>
+            <div className="flex flex-row items-center justify-center mx-2 my-2 py-2 px-4 bg-black bg-opacity-40 rounded">
+              <i className="fab fa-node-js mr-1 text-xl text-gray-100" />
+              <span className="ml-1 font-proxima font-semibold text-xl text-gray-300">NODEJS</span>
+            </div>
+            <div className="flex flex-row items-center justify-center mx-2 my-2 py-2 px-4 bg-black bg-opacity-40 rounded">
+              <i className="fas fa-elephant mr-1 text-xl text-gray-100" />
+              <span className="ml-1 font-proxima font-semibold text-xl text-gray-300">POSTGRESQL</span>
+            </div>
+            <div className="flex flex-row items-center justify-center mx-2 my-2 py-2 px-4 bg-black bg-opacity-40 rounded">
+              <i className="fab fa-react mr-1 text-xl text-gray-100" />
+              <span className="ml-1 font-proxima font-semibold text-xl text-gray-300">REACTJS</span>
+            </div>
+            <div className="flex flex-row items-center justify-center mx-2 my-2 py-2 px-4 bg-black bg-opacity-40 rounded">
+              <i className="fas fa-triangle mr-1 text-xl text-gray-100" />
+              <span className="ml-1 font-proxima font-semibold text-xl text-gray-300">NEXTJS</span>
+            </div>
+            <div className="flex flex-row items-center justify-center mx-2 my-2 py-2 px-4 bg-black bg-opacity-40 rounded">
+              <i className="fab fa-js-square mr-1 text-xl text-gray-100" />
+              <span className="ml-1 font-proxima font-semibold text-xl text-gray-300">JSX</span>
+            </div>
+            <div className="flex flex-row items-center justify-center mx-2 my-2 py-2 px-4 bg-black bg-opacity-40 rounded">
+              <i className="fab fa-html5 mr-1 text-xl text-gray-100" />
+              <span className="ml-1 font-proxima font-semibold text-xl text-gray-300">HTML5</span>
+            </div>
+            <div className="flex flex-row items-center justify-center mx-2 my-2 py-2 px-4 bg-black bg-opacity-40 rounded">
+              <i className="fab fa-css3-alt mr-1 text-xl text-gray-100" />
+              <span className="ml-1 font-proxima font-semibold text-xl text-gray-300">CSS3</span>
+            </div>
+            <div className="flex flex-row items-center justify-center mx-2 my-2 py-2 px-4 bg-black bg-opacity-40 rounded">
+              <i className="fas fa-wave-sine mr-1 text-xl text-gray-100" />
+              <span className="ml-1 font-proxima font-semibold text-xl text-gray-300">TAILWINDCSS</span>
+            </div>
+            <div className="flex flex-row items-center justify-center mx-2 my-2 py-2 px-4 bg-black bg-opacity-40 rounded">
+              <i className="fab fa-docker mr-1 text-xl text-gray-100" />
+              <span className="ml-1 font-proxima font-semibold text-xl text-gray-300">DOCKER</span>
+            </div>
+            <div className="flex flex-row items-center justify-center mx-2 my-2 py-2 px-4 bg-black bg-opacity-40 rounded">
+              <i className="fab fa-github mr-1 text-xl text-gray-100" />
+              <span className="ml-1 font-proxima font-semibold text-xl text-gray-300">ACTIONS</span>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col items-start justify-center mt-4">
+          <span className="mb-2 font-proxima font-bold text-3xl text-gray-300">TECHNOLOGY</span>
+          <div className="flex flex-row items-center justify-start flex-wrap mt-2">
+            <div className="flex flex-row items-center justify-center mx-2 py-2 px-4 bg-black bg-opacity-40 rounded">
+              <i className="fab fa-github mr-1 text-xl text-gray-100" />
+              <span className="ml-1 font-proxima font-semibold text-xl text-gray-300">GITHUB</span>
+            </div>
+            <div className="flex flex-row items-center justify-center mx-2 my-2 py-2 px-4 bg-black bg-opacity-40 rounded">
+              <i className="fab fa-figma mr-1 text-xl text-gray-100" />
+              <span className="ml-1 font-proxima font-semibold text-xl text-gray-300">FIGMA</span>
+            </div>
+            <div className="flex flex-row items-center justify-center mx-2 my-2 py-2 px-4 bg-black bg-opacity-40 rounded">
+              <i className="fab fa-windows mr-1 text-xl text-gray-100" />
+              <span className="ml-1 font-proxima font-semibold text-xl text-gray-300">WINDOWS</span>
+            </div>
+            <div className="flex flex-row items-center justify-center mx-2 my-2 py-2 px-4 bg-black bg-opacity-40 rounded">
+              <i className="fab fa-ubuntu mr-1 text-xl text-gray-100" />
+              <span className="ml-1 font-proxima font-semibold text-xl text-gray-300">UBUNTU</span>
+            </div>
+            <div className="flex flex-row items-center justify-center mx-2 my-2 py-2 px-4 bg-black bg-opacity-40 rounded">
+              <i className="far fa-cube mr-1 text-xl text-gray-100" />
+              <span className="ml-1 font-proxima font-semibold text-xl text-gray-300">JETBRAINS</span>
+            </div>
+            <div className="flex flex-row items-center justify-center mx-2 my-2 py-2 px-4 bg-black bg-opacity-40 rounded">
+              <i className="fal fa-atom mr-1 text-xl text-gray-100" />
+              <span className="ml-1 font-proxima font-semibold text-xl text-gray-300">ATOM</span>
+            </div>
+            <div className="flex flex-row items-center justify-center mx-2 my-2 py-2 px-4 bg-black bg-opacity-40 rounded">
+              <i className="fab fa-font-awesome mr-1 text-xl text-gray-100" />
+              <span className="ml-1 font-proxima font-semibold text-xl text-gray-300">FONT AWESOME</span>
+            </div>
+            <div className="flex flex-row items-center justify-center mx-2 my-2 py-2 px-4 bg-black bg-opacity-40 rounded">
+              <i className="fab fa-python mr-1 text-xl text-gray-100" />
+              <span className="ml-1 font-proxima font-semibold text-xl text-gray-300">PYPI</span>
+            </div>
+            <div className="flex flex-row items-center justify-center mx-2 my-2 py-2 px-4 bg-black bg-opacity-40 rounded">
+              <i className="fab fa-npm mr-1 text-xl text-gray-100" />
+              <span className="ml-1 font-proxima font-semibold text-xl text-gray-300">NPM</span>
+            </div>
+            <div className="flex flex-row items-center justify-center mx-2 my-2 py-2 px-4 bg-black bg-opacity-40 rounded">
+              <i className="fab fa-raspberry-pi mr-1 text-xl text-gray-100" />
+              <span className="ml-1 font-proxima font-semibold text-xl text-gray-300">RASPBERRY PI</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Default>
+  )
+
+}
+
+export default Skills;
