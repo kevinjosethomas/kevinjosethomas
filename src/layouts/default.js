e40ea5239bceda5ee104afb6bc37f921bd9bf64f
TrustedMercury <kevin.jt2007@gmail.com>
2021-02-12 13:51:10 -0500

    Updated default layout, moved background to <body>

--- src/layouts/default.js
+++ src/layouts/default.js
@@ -3,11 +3,9 @@ import { Navbar } from "../components/Navbar";
 export const Default = (props) => {
 
   return (
-    <div className="bg flex flex-col items-start justify-center w-full h-full min-h-screen">
-      <div className="flex flex-col items-start justify-center w-full h-screen">
-        <Navbar />
-        { props.children }
-      </div>
+    <div className="flex flex-col items-start justify-center w-full h-full overflow-x-hidden">
+      <Navbar />
+      { props.children }
     </div>
   )
 
