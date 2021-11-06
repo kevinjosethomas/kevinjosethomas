ddf19d3e1ceb9c21ff9bc575032e5027d2cf10af
Kevin Thomas <kevin.jt2007@gmail.com>
2021-11-06 00:36:22 -0700

    feat: added socials

--- src/pages/home/Intro.jsx
+++ src/pages/home/Intro.jsx
@@ -12,12 +12,28 @@ export default function Intro(props) {
   }, []);
 
   return (
-    <div className="flex flex-col items-start justify-start w-screen h-screen p-5 bg-black">
-      <div className="flex flex-row items-center justify-start space-x-2">
-        <i className="fal fa-clock text-xl text-white" />
+    <div className="flex flex-col items-start justify-start w-screen h-screen p-10 space-y-3 bg-black">
+      <div className="flex flex-col items-start justify-start space-y-2">
+        <div className="flex flex-row items-center justify-start space-x-4">
+          <i className="fal fa-clock text-xl text-white" />
+          <p className="text-xl text-white text-opacity-90">
+            {time.format("MMMM Do YYYY")} • {time.format("hh:mm:ss a")}
+          </p>
+        </div>
+      </div>
+      <div className="flex flex-col items-start justify-start space-y-1">
         <p className="text-xl text-white text-opacity-90">
-          {time.format("MMMM Do YYYY")} • {time.format("hh:mm:ss a")}
+          Welcome to my portfolio v2.1.0 (@trustedmercury/latest)
         </p>
+        <div className="flex flex-col items-start justify-start">
+          <p className="text-xl text-white text-opacity-90">* Discord: https://dsc.gg/tm</p>
+          <p className="text-xl text-white text-opacity-90">
+            * GitHub: https://github.com/trustedmercury
+          </p>
+          <p className="text-xl text-white text-opacity-90">
+            * Twitter: https://twitter.com/trustedmercury
+          </p>
+        </div>
       </div>
     </div>
   );
