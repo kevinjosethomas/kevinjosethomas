e243fba60228c89089efca88182d998e2312f13d
Kevin Thomas <kevin.jt2007@gmail.com>
2021-11-06 01:03:30 -0700

    feat: animated home page & added typist

--- src/pages/home/Intro.jsx
+++ src/pages/home/Intro.jsx
@@ -1,7 +1,11 @@
 import "moment-timezone";
 import axios from "axios";
 import moment from "moment";
+import Typist from "react-typist";
+import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
+
+import Link from "./Link";
 
 export default function Intro(props) {
   const [weather, setWeather] = useState();
@@ -27,27 +31,39 @@ export default function Intro(props) {
 
   return (
     <div className="flex flex-col items-start justify-start w-screen h-screen p-10 space-y-3 bg-black">
-      <div className="flex flex-col items-start justify-start space-y-2">
+      <div className="flex flex-col items-start justify-start space-y-1">
         <div className="flex flex-row items-center justify-start space-x-4">
-          <i className="fal fa-clock text-xl text-white" />
+          <i className="fal fa-clock w-[25px] text-xl text-blue-300 text-center" />
           <p className="text-xl text-white text-opacity-90">
             {time.format("MMMM Do YYYY")} • {time.format("hh:mm:ss a")}
           </p>
         </div>
+        {weather && (
+          <div className="flex flex-row items-center justify-start space-x-4">
+            <i className="fal fa-clouds w-[25px] text-xl text-blue-300 text-center" />
+            <p className="text-xl text-white text-opacity-90">
+              Feels like {Math.ceil(weather.main.feels_like)}°C in Vancouver
+            </p>
+          </div>
+        )}
       </div>
       <div className="flex flex-col items-start justify-start space-y-1">
-        <p className="text-xl text-white text-opacity-90">
-          Welcome to my portfolio v2.1.0 (@trustedmercury/latest)
-        </p>
-        <div className="flex flex-col items-start justify-start">
-          <p className="text-xl text-white text-opacity-90">* Discord: https://dsc.gg/tm</p>
+        <Typist avgTypingDelay={35} stdTypingDelay={30} cursor={{ show: false }}>
           <p className="text-xl text-white text-opacity-90">
-            * GitHub: https://github.com/trustedmercury
+            Welcome to my portfolio v2.1.0 (
+            <span className="text-blue-300">@trustedmercury/latest</span>)
           </p>
-          <p className="text-xl text-white text-opacity-90">
-            * Twitter: https://twitter.com/trustedmercury
-          </p>
-        </div>
+        </Typist>
+        <motion.div
+          className="flex flex-col items-start justify-start"
+          initial={{ display: "none" }}
+          animate={{ display: "flex" }}
+          transition={{ delay: 3 }}
+        >
+          <Link label="Discord" link="https://dsc.gg/tm" />
+          <Link label="GitHub" link="https://github.com/trustedmercury" />
+          <Link label="Twitter" link="https://twitter.com/trustedmercury" />
+        </motion.div>
       </div>
     </div>
   );
