55a230bec1f626cd5d0c283f7e88aad031bb1d65
Kevin Thomas <kevin.jt2007@gmail.com>
2021-11-06 13:08:53 -0700

    feat: added dm sans font

--- src/pages/_document.p.jsx
+++ src/pages/_document.p.jsx
@@ -14,7 +14,7 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Ubuntu+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&family=Ubuntu+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap"
           />
 
           <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.15.4/css/all.css" />
