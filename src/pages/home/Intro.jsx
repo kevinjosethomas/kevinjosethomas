084ce642772e6465b9edc84df434be58207aea80
Kevin Thomas <kevin.jt2007@gmail.com>
2021-11-06 01:07:28 -0700

    feat: perfected animations and rendered weather on server side

--- src/pages/home/Intro.jsx
+++ src/pages/home/Intro.jsx
@@ -1,5 +1,4 @@
 import "moment-timezone";
-import axios from "axios";
 import moment from "moment";
 import Typist from "react-typist";
 import { motion } from "framer-motion";
@@ -8,20 +7,7 @@ import { useEffect, useState } from "react";
 import Link from "./Link";
 
 export default function Intro(props) {
-  const [weather, setWeather] = useState();
   const [time, setTime] = useState(moment.tz(new Date(), "America/Vancouver"));
-
-  useEffect(() => {
-    (async () => {
-      const [response, error] = await fetchWeather();
-
-      if (error) {
-        return;
-      }
-
-      setWeather(response);
-    })();
-  }, []);
 
   useEffect(() => {
     setInterval(() => {
@@ -38,19 +24,17 @@ export default function Intro(props) {
             {time.format("MMMM Do YYYY")} • {time.format("hh:mm:ss a")}
           </p>
         </div>
-        {weather && (
-          <div className="flex flex-row items-center justify-start space-x-4">
-            <i className="fal fa-clouds w-[25px] text-xl text-blue-300 text-center" />
-            <p className="text-xl text-white text-opacity-90">
-              Feels like {Math.ceil(weather.main.feels_like)}°C in Vancouver
-            </p>
-          </div>
-        )}
+        <div className="flex flex-row items-center justify-start space-x-4">
+          <i className="fal fa-clouds w-[25px] text-xl text-blue-300 text-center" />
+          <p className="text-xl text-white text-opacity-90">
+            Feels like {Math.ceil(props.weather.main.feels_like)}°C in Vancouver
+          </p>
+        </div>
       </div>
       <div className="flex flex-col items-start justify-start space-y-1">
-        <Typist avgTypingDelay={35} stdTypingDelay={30} cursor={{ show: false }}>
+        <Typist startDelay={500} avgTypingDelay={35} stdTypingDelay={30} cursor={{ show: false }}>
           <p className="text-xl text-white text-opacity-90">
-            Welcome to my portfolio v2.1.0 (
+            Welcome to my portfolio v2.1.0 <Typist.Delay ms={500} />(
             <span className="text-blue-300">@trustedmercury/latest</span>)
           </p>
         </Typist>
@@ -58,7 +42,7 @@ export default function Intro(props) {
           className="flex flex-col items-start justify-start"
           initial={{ display: "none" }}
           animate={{ display: "flex" }}
-          transition={{ delay: 3 }}
+          transition={{ delay: 4 }}
         >
           <Link label="Discord" link="https://dsc.gg/tm" />
           <Link label="GitHub" link="https://github.com/trustedmercury" />
@@ -68,13 +52,3 @@ export default function Intro(props) {
     </div>
   );
 }
-
-const fetchWeather = async () => {
-  try {
-    const response = await axios.get("/api/weather");
-
-    return [response.data.payload, null];
-  } catch (e) {
-    return [null, e];
-  }
-};
--- src/pages/index.p.jsx
+++ src/pages/index.p.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useState } from "react";
 
 import Intro from "./home/Intro";
@@ -6,8 +7,20 @@ export default function Home(props) {
   const [initialized, setInitialized] = useState(false);
 
   if (!initialized) {
-    return <Intro />;
+    return <Intro weather={props.weather} />;
   }
 
   return <div className="flex flex-col items-start justify-start w-screen h-screen"></div>;
+}
+
+export async function getServerSideProps(ctx) {
+  const response = await axios.get(
+    `https://api.openweathermap.org/data/2.5/weather?q=${process.env.OPENWEATHER_LOCATION}&appid=${process.env.OPENWEATHER_API_KEY}&units=metric`
+  );
+
+  return {
+    props: {
+      weather: response.data,
+    },
+  };
 }
