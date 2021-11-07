1d43f03aed8374d5b62347951593b3c500be5219
Kevin Thomas <kevin.jt2007@gmail.com>
2021-11-07 15:17:24 -0800

    feat: completed awards page

---
+++ src/pages/awards/components/Award.jsx
@@ -0,0 +1,21 @@
+export default function Award(props) {
+  return (
+    <div className="flex flex-row items-center justify-start w-full hover:bg-white hover:bg-opacity-5 border-[1px] border-white border-opacity-10 rounded transition duration-300">
+      <div className="flex flex-col items-start justify-center h-28 w-[250px] pl-5 border-[1px] border-white border-opacity-10">
+        <p className="terminal text-xl text-white text-opacity-90 leading-snug">info</p>
+        <p className="text-lg text-white text-opacity-80 leading-snug">
+          {props.title}
+          <br />
+          {props.subtitle}
+        </p>
+      </div>
+      <div className="flex flex-col items-start justify-center h-28 w-full pl-5 border-[1px] border-white border-opacity-5">
+        {props.lines.map((line, index) => (
+          <p key={index} className="text-lg text-white text-opacity-80 leading-snug">
+            {line}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
+}
--- src/pages/awards/index.p.jsx
+++ src/pages/awards/index.p.jsx
@@ -1,5 +1,34 @@
+import Award from "./components/Award";
 import Default from "ui/layouts/Default";
 
 export default function Awards(props) {
-  return <Default></Default>;
+  return (
+    <Default>
+      <div className="flex flex-col items-start justify-start w-full space-y-4">
+        <h1 className="font-bold text-4xl text-white text-opacity-80 tracking-[-0.02rem]">
+          🏆 Awards
+        </h1>
+        <div className="flex flex-col items-start justify-start w-full space-y-2">
+          <Award
+            title="BorderHacks"
+            subtitle="Sep 25-26 2021"
+            lines={[
+              "Youngest winner among over 300 participants. 2 awards:",
+              "• Optimizing Community Services with Open Data ($500)",
+              "• Best UI / UX Design Award (Sketch, Thunkable & Axure licenses)",
+            ]}
+          />
+          <Award
+            title="CCC - 64/75"
+            subtitle="Feb 17 2021"
+            lines={[
+              "Youngest participant in the Canadian Computing Competiton 2021",
+              "• Completed all challenges in under 2 hours with Python 3",
+              "• Scored 45/45 in the first 3 challenges, 19/30 in the last 2",
+            ]}
+          />
+        </div>
+      </div>
+    </Default>
+  );
 }
