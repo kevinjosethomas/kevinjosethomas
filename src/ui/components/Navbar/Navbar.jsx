2af07584efdab154c16db893d9d2dd0db955d3b5
Kevin Thomas <kevin.jt2007@gmail.com>
2021-11-07 11:23:11 -0800

    feat: changed navigation separator

--- src/ui/components/Navbar/Navbar.jsx
+++ src/ui/components/Navbar/Navbar.jsx
@@ -3,11 +3,12 @@ import Navigation from "./components/Navigation";
 
 export default function Navbar(props) {
   return (
-    <div className="flex flex-col items-start justify-start w-full py-10">
+    <div className="flex flex-col items-start justify-start w-full py-10 space-y-4">
       <div className="flex flex-row items-center justify-between w-full">
         <Navigation />
         <Socials />
       </div>
+      <div className="w-full h-0.5 bg-white bg-opacity-10" />
     </div>
   );
 }
--- src/ui/components/Navbar/components/Navigation.jsx
+++ src/ui/components/Navbar/components/Navigation.jsx
@@ -26,14 +26,11 @@ export default function Navigation(props) {
   const router = useRouter();
 
   return (
-    <div className="flex flex-col items-start justify-start w-full space-y-4">
-      <div className="terminal flex flex-row items-center justify-start space-x-8">
-        {elements.map((el, ind) => (
-          <Element key={ind} {...el} active={router.pathname === el.href} />
-        ))}
-        <Dropdown />
-      </div>
-      <div className="w-full h-0.5 bg-white bg-opacity-10" />
+    <div className="terminal flex flex-row items-center justify-start space-x-8">
+      {elements.map((el, ind) => (
+        <Element key={ind} {...el} active={router.pathname === el.href} />
+      ))}
+      <Dropdown />
     </div>
   );
 }
