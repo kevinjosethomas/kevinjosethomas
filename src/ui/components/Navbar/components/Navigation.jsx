4ef64f57acd56cba31b6cf41c5acea66e3636c29
Kevin Thomas <kevin.jt2007@gmail.com>
2021-11-07 10:11:32 -0800

    feat: added navigation

---
+++ src/ui/components/Navbar/components/Navigation.jsx
@@ -0,0 +1,36 @@
+import Element from "./Element";
+import { useRouter } from "next/router";
+
+export default function Navigation(props) {
+  const elements = [
+    {
+      label: "/",
+      href: "/",
+    },
+    {
+      label: "/projects",
+      href: "/projects",
+    },
+    {
+      label: "/awards",
+      href: "/awards",
+    },
+    {
+      label: "/stack",
+      href: "/stack",
+    },
+  ];
+
+  const router = useRouter();
+
+  return (
+    <div className="flex flex-col items-start justify-start w-full space-y-4">
+      <div className="terminal flex flex-row items-center justify-start space-x-8">
+        {elements.map((el, ind) => (
+          <Element key={ind} {...el} active={router.pathname === el.href} />
+        ))}
+      </div>
+      <div className="w-full h-0.5 bg-white bg-opacity-10" />
+    </div>
+  );
+}
