f3e1f071afc579ed629777be6ddf82aeec7f63f2
Kevin Thomas <kevin.jt2007@gmail.com>
2021-11-07 11:28:55 -0800

    feat: reduced width of page

--- src/ui/layouts/Default.jsx
+++ src/ui/layouts/Default.jsx
@@ -3,7 +3,7 @@ import Navbar from "ui/components/Navbar/Navbar";
 export default function Default(props) {
   return (
     <div className="flex flex-col items-center justify-start w-full overflow-x-hidden bg-black">
-      <div className="flex flex-col items-center justify-start w-[1000px] min-h-screen h-full overflow-x-hidden">
+      <div className="flex flex-col items-center justify-start w-[900px] min-h-screen h-full overflow-x-hidden">
         <Navbar />
         <div className="flex flex-col items-start justify-start w-full">{props.children}</div>
       </div>
