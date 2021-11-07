895529f4f64dbe5ebb033bc336474b223ebe4b42
Kevin Thomas <kevin.jt2007@gmail.com>
2021-11-07 10:22:31 -0800

    feat: added stack page

---
+++ src/pages/stack/index.p.jsx
@@ -0,0 +1,8 @@
+import axios from "axios";
+import { Fragment, useEffect, useState } from "react";
+
+import Default from "ui/layouts/Default";
+
+export default function Home(props) {
+  return <Default></Default>;
+}
