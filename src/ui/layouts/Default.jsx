be3ca32bb6d069ae204cec92edaed1d999426d49
Kevin Thomas <kevin.jt2007@gmail.com>
2021-11-07 10:54:51 -0800

    fix: fixed overflow

--- src/ui/layouts/Default.jsx
+++ src/ui/layouts/Default.jsx
@@ -3,11 +3,9 @@ import Navbar from "ui/components/Navbar/Navbar";
 export default function Default(props) {
   return (
     <div className="flex flex-col items-center justify-start w-full overflow-x-hidden bg-black">
-      <div className="flex flex-col items-center justify-start w-[1000px] h-full overflow-x-hidden">
+      <div className="flex flex-col items-center justify-start w-[1000px] min-h-screen h-full overflow-x-hidden">
         <Navbar />
-        <div className="flex flex-col items-start justify-start w-full h-full">
-          {props.children}
-        </div>
+        <div className="flex flex-col items-start justify-start w-full">{props.children}</div>
       </div>
     </div>
   );
