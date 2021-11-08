097d22fdb709bb44095921132dc84905dd24a855
Kevin Thomas <kevin.jt2007@gmail.com>
2021-11-07 16:24:02 -0800

    chore: minor padding changes

--- src/pages/awards/components/Award.jsx
+++ src/pages/awards/components/Award.jsx
@@ -1,7 +1,7 @@
 export default function Award(props) {
   return (
     <div className="flex flex-row items-center justify-start w-full hover:bg-white hover:bg-opacity-5 border-[1px] border-white border-opacity-10 rounded transition duration-300">
-      <div className="flex flex-col items-start justify-center h-28 w-[250px] pl-5 border-[1px] border-white border-opacity-10">
+      <div className="flex flex-col items-start justify-center h-28 min-w-[200px] p-5 border-[1px] border-white border-opacity-10">
         <p className="terminal text-xl text-white text-opacity-90 leading-snug">info</p>
         <p className="text-lg text-white text-opacity-80 leading-snug">
           {props.title}
@@ -9,7 +9,7 @@ export default function Award(props) {
           {props.subtitle}
         </p>
       </div>
-      <div className="flex flex-col items-start justify-center h-28 w-full pl-5 border-[1px] border-white border-opacity-5">
+      <div className="flex flex-col items-start justify-center h-28 w-full p-5 border-[1px] border-white border-opacity-5">
         {props.lines.map((line, index) => (
           <p key={index} className="text-lg text-white text-opacity-80 leading-snug">
             {line}
--- src/pages/stack/components/Technology.jsx
+++ src/pages/stack/components/Technology.jsx
@@ -7,11 +7,11 @@ export default function Technology(props) {
         <img src={`/tech/${props.icon}`} draggable="false" className="w-12" />
       </div>
       <div className="flex flex-row items-center justify-start w-full min-h-[8rem]">
-        <div className="flex flex-col items-start justify-center h-32 w-[175px] pl-5 border-[1px] border-white border-opacity-5">
+        <div className="flex flex-col items-start justify-center h-32 w-[175px] p-5 border-[1px] border-white border-opacity-5">
           <span className="terminal text-xl text-white text-opacity-90 leading-snug">name</span>
           <span className="text-lg text-white text-opacity-80 leading-snug">{props.name}</span>
         </div>
-        <div className="flex flex-col items-start justify-center h-32 w-full pl-5 border-[1px] border-white border-opacity-5">
+        <div className="flex flex-col items-start justify-center h-32 w-full p-5 border-[1px] border-white border-opacity-5">
           <span className="terminal text-xl text-white text-opacity-90 leading-snug">use case</span>
           <span className="text-lg text-white text-opacity-80 leading-snug">{props.for}</span>
         </div>
