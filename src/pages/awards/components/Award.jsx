39a7b3932c36f88b59b399507b187da648304fea
Kevin Thomas <kevin.jt2007@gmail.com>
2021-11-07 16:28:19 -0800

    feat: animated awards page

--- src/pages/awards/components/Award.jsx
+++ src/pages/awards/components/Award.jsx
@@ -1,6 +1,13 @@
+import { motion } from "framer-motion";
+
 export default function Award(props) {
   return (
-    <div className="flex flex-row items-center justify-start w-full hover:bg-white hover:bg-opacity-5 border-[1px] border-white border-opacity-10 rounded transition duration-300">
+    <motion.div
+      className="flex flex-row items-center justify-start w-full hover:bg-white hover:bg-opacity-5 border-[1px] border-white border-opacity-10 rounded transition duration-300"
+      initial={{ y: 10, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ duration: 0.2, delay: 0.1 * props.index }}
+    >
       <div className="flex flex-col items-start justify-center h-28 min-w-[200px] p-5 border-[1px] border-white border-opacity-10">
         <p className="terminal text-xl text-white text-opacity-90 leading-snug">info</p>
         <p className="text-lg text-white text-opacity-80 leading-snug">
@@ -16,6 +23,6 @@ export default function Award(props) {
           </p>
         ))}
       </div>
-    </div>
+    </motion.div>
   );
 }
--- src/pages/awards/index.p.jsx
+++ src/pages/awards/index.p.jsx
@@ -2,6 +2,27 @@ import Award from "./components/Award";
 import Default from "ui/layouts/Default";
 
 export default function Awards(props) {
+  const awards = [
+    {
+      title: "BorderHacks",
+      subtitle: "Sep 25-26 2021",
+      lines: [
+        "Hackathon; Youngest winner among over 300 participants. 2 awards:",
+        "• Optimizing Community Services with Open Data ($500)",
+        "• Best UI / UX Design Award (Sketch, Thunkable & Axure licenses)",
+      ],
+    },
+    {
+      title: "CCC - 64/75",
+      subtitle: "Feb 17 2021",
+      lines: [
+        "Youngest participant in the Canadian Computing Competiton 2021",
+        "• Completed all challenges in under 2 hours with Python 3",
+        "• Scored 45/45 in the first 3 challenges, 19/30 in the last 2",
+      ],
+    },
+  ];
+
   return (
     <Default>
       <div className="flex flex-col items-start justify-start w-full space-y-4">
@@ -9,24 +30,9 @@ export default function Awards(props) {
           🏆 Awards
         </h1>
         <div className="flex flex-col items-start justify-start w-full space-y-2">
-          <Award
-            title="BorderHacks"
-            subtitle="Sep 25-26 2021"
-            lines={[
-              "Hackathon; Youngest winner among over 300 participants. 2 awards:",
-              "• Optimizing Community Services with Open Data ($500)",
-              "• Best UI / UX Design Award (Sketch, Thunkable & Axure licenses)",
-            ]}
-          />
-          <Award
-            title="CCC - 64/75"
-            subtitle="Feb 17 2021"
-            lines={[
-              "Youngest participant in the Canadian Computing Competiton 2021",
-              "• Completed all challenges in under 2 hours with Python 3",
-              "• Scored 45/45 in the first 3 challenges, 19/30 in the last 2",
-            ]}
-          />
+          {awards.map((award, index) => (
+            <Award key={index} index={index} {...award} />
+          ))}
         </div>
       </div>
     </Default>
