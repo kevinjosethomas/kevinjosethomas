57252ccad55214459c87e3e683b47261532807c8
Kevin Thomas <kevin.jt2007@gmail.com>
2021-11-08 19:41:23 -0800

    feat: added meta tags and favicon

--- src/pages/_app.p.jsx
+++ src/pages/_app.p.jsx
@@ -1,5 +1,10 @@
-import AnimatedCursor from "react-animated-cursor";
+import Head from "next/head";
+import dynamic from "next/dynamic";
 import { AnimateSharedLayout } from "framer-motion";
+
+const AnimatedCursor = dynamic(() => import("react-animated-cursor"), {
+  ssr: false,
+});
 
 import "ui/styles/custom.css";
 import "tailwindcss/tailwind.css";
@@ -7,6 +12,37 @@ import "tailwindcss/tailwind.css";
 export default function App({ Component, pageProps }) {
   return (
     <AnimateSharedLayout>
+      <Head>
+        <meta
+          name="keywords"
+          content="kevin, thomas, kevin thomas, kevin jose thomas, kevin thomas codes, kevin codes, portfolio, kevin thomas portfolio, kevin thomas website"
+        />
+        <meta
+          name="description"
+          content="I'm Kevin Thomas, a 14 year old full-stack developer and an aspiring tech entrepreneur. Experienced in py, js, sql, react & more!"
+        />
+
+        <meta property="og:image" content="/embed.png" />
+        <meta
+          property="og:description"
+          content="I'm Kevin Thomas, a 14 year old full-stack developer and an aspiring tech entrepreneur. Experienced in py, js, sql, react & more!"
+        />
+
+        <meta property="twitter:image" content="/embed.png" />
+        <meta
+          property="twitter:description"
+          content="I'm Kevin Thomas, a 14 year old full-stack developer and an aspiring tech entrepreneur. Experienced in py, js, sql, react & more!"
+        />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="kevinthomas.codes" />
+        <meta property="og:url" content="https://kevinthomas.codes/" />
+
+        <meta property="twitter:card" content="summary" />
+        <meta property="twitter:url" content="https://kevinthomas.codes/" />
+
+        <link rel="shortcut icon" type="image/svg+xml" href="/favicon.ico" />
+      </Head>
       <Component {...pageProps} />
       <AnimatedCursor
         innerSize={4}
