a21b8127c0f860bdd44feaac2c4305a119b857d3
Kevin Thomas <kevin.jt2007@gmail.com>
2021-11-08 19:17:27 -0800

    feat: added custom cursor

--- src/pages/_app.p.jsx
+++ src/pages/_app.p.jsx
@@ -1,3 +1,4 @@
+import AnimatedCursor from "react-animated-cursor";
 import { AnimateSharedLayout } from "framer-motion";
 
 import "ui/styles/custom.css";
@@ -7,6 +8,14 @@ export default function App({ Component, pageProps }) {
   return (
     <AnimateSharedLayout>
       <Component {...pageProps} />
+      <AnimatedCursor
+        innerSize={4}
+        outerSize={32}
+        color="147, 197, 253"
+        outerAlpha={0.2}
+        innerScale={1}
+        outerScale={1.5}
+      />
     </AnimateSharedLayout>
   );
 }
