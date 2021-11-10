6b4b428a049de58708621efbfc18d8f524084515
Kevin Thomas <kevin.jt2007@gmail.com>
2021-11-09 18:08:09 -0800

    feat: made projects page responsive

--- src/pages/projects/components/Project.jsx
+++ src/pages/projects/components/Project.jsx
@@ -5,12 +5,12 @@ export default function Project(props) {
     <motion.a
       target="_blank"
       href={props.href}
-      className="flex flex-row items-center justify-start w-full hover:bg-white hover:bg-opacity-5 border-[1px] border-white border-opacity-10 rounded transition duration-300"
+      className="flex flex-col md:flex-row items-center justify-start w-full hover:bg-white hover:bg-opacity-5 border-[1px] border-white border-opacity-10 rounded transition duration-300"
       initial={{ y: 10, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       transition={{ duration: 0.2, delay: 0.1 * props.index }}
     >
-      <div className="flex flex-col items-start justify-center h-28 min-w-[232px] p-5 border-[1px] border-white border-opacity-10">
+      <div className="flex flex-col items-start justify-center md:h-28 w-full md:w-auto md:min-w-[232px] p-5 border-[1px] border-white border-opacity-10">
         <div className="flex flex-row items-center justify-center space-x-2">
           <div
             className={`w-2 h-2 ${
@@ -26,7 +26,7 @@ export default function Project(props) {
         <p className="text-lg text-white text-opacity-80 leading-snug">{props.subtitle}</p>
         <p className="text-sm text-white text-opacity-80 leading-snug">{props.date}</p>
       </div>
-      <div className="flex flex-col items-start justify-center h-28 w-full p-5 border-[1px] border-white border-opacity-5">
+      <div className="flex flex-col items-start justify-center md:h-28 w-full p-5 border-[1px] border-white border-opacity-5">
         <p className="text-lg text-white text-opacity-80 leading-snug">{props.description}</p>
       </div>
     </motion.a>
