f5cacea0e9a6fa1eafc71bbdb7996f9019f66d6b
Kevin Thomas <kevin.jt2007@gmail.com>
2022-12-19 00:59:32 -0800

    feat: initialized App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Home from "pages/Home";
+
+import "ui/styles/tailwind.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="grid h-full min-h-screen w-screen bg-black">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
