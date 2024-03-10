ab892d94e6f7288116137d4e7b9566c4f1c4bc31
Kevin Thomas <kevin.jt2007@gmail.com>
2024-03-09 20:09:57 -0800

    feat: finished home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,92 @@
+"use client";
+
+import Image from "next/image";
+import { motion } from "framer-motion";
+
+import Presence from "@/ui/components/Presence";
+
 export default function Home() {
-  return <div></div>;
+  const socials = [
+    {
+      label: "GitHub",
+      href: "https://github.com/kevinjosethomas",
+    },
+    {
+      label: "Linkedin",
+      href: "https://linkedin.com/in/kevinjosethomas",
+    },
+  ];
+
+  return (
+    <div className="flex flex-col w-1/2 items-start text-white text-md gap-4 2xl:text-xl 3xl:text-2xl text-opacity-80">
+      <motion.p
+        initial={{ opacity: 0, y: 10 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.3 }}
+      >
+        Hey!{" "}
+        <Image
+          src={"/icons/wave.png"}
+          alt="Wave"
+          className="mx-1 inline h-6"
+          width={24}
+          height={24}
+          draggable="false"
+        />{" "}
+        I'm a high school junior from Vancouver with a passion for computer
+        science and software development.
+      </motion.p>
+      <motion.p
+        initial={{ opacity: 0, y: 10 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.3, delay: 0.1 }}
+      >
+        Over the last four years, I have built proficiency in Python,
+        TypesSript, Java, PostgreSQL and a couple other languages and
+        frameworks. I use my full-stack experience to work on developing
+        efficient software with modern design and exceptional user experiences.
+      </motion.p>
+      <motion.p
+        initial={{ opacity: 0, y: 10 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.3, delay: 0.2 }}
+      >
+        Now, most of my life is off my computer: exploring yvr, volunteering, or
+        playing badminton and grinding for school. I spend most of my time
+        trying out new things, making new friends and preparing for
+        post-secondary.
+      </motion.p>
+      <motion.p
+        initial={{ opacity: 0, y: 10 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.3, delay: 0.3 }}
+      >
+        Although most of my time is in school, i still enjoy learning about new
+        technology, participating in hackathons and occasionally freelancing or
+        working on a few side projects.
+      </motion.p>
+      <motion.div
+        initial={{ opacity: 0, y: 10 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.3, delay: 0.4 }}
+        className="flex flex-col md:flex-row"
+      >
+        Reach out:{" "}
+        <div className="flex items-center gap-2 md:ml-2">
+          {socials.map((s, i) => (
+            <div
+              key={i}
+              className="relative opacity-80 transition duration-300 hover:opacity-100"
+            >
+              <a href={s.href} target="_blank" rel="noreferrer">
+                {s.label}
+              </a>
+              <div className="absolute bottom-0.5 h-0.5 w-full bg-white" />
+            </div>
+          ))}
+        </div>
+      </motion.div>
+      <Presence />
+    </div>
+  );
 }
---
+++ src/ui/components/Presence.tsx
@@ -0,0 +1,72 @@
+import Image from "next/image";
+import { motion } from "framer-motion";
+import { Fragment, memo } from "react";
+import { useLanyardWS } from "use-lanyard";
+
+const Presence = memo(function () {
+  const data = useLanyardWS("418707912836382721");
+
+  return (
+    <Fragment>
+      {data?.spotify ? (
+        <motion.a
+          target="_blank"
+          href="https://open.spotify.com/user/ock5719fh26056w67awzv7rty"
+          initial={{ opacity: 0, y: 10 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.4, delay: 0.5 }}
+          className="flex w-72 select-none items-center justify-start gap-3 overflow-hidden rounded-lg border-2 border-white border-opacity-5 bg-white bg-opacity-5 p-3 2xl:w-80 3xl:mt-6"
+        >
+          {data.spotify.album_art_url && (
+            <img
+              src={data?.spotify.album_art_url}
+              alt="Album Art"
+              className="w-10 rounded-md 2xl:w-12"
+            />
+          )}
+          <div className="flex w-56 flex-col overflow-hidden whitespace-nowrap leading-snug text-white text-opacity-75 2xl:w-64">
+            <p className="overflow-hidden text-ellipsis 2xl:text-lg 3xl:text-xl">
+              Listening to{" "}
+              <span className="font-semibold text-opacity-90">
+                {data.spotify.song}
+              </span>
+            </p>
+            <div className="flex items-center gap-1 text-xs 2xl:text-sm">
+              <Image
+                src="/icons/spotify.svg"
+                alt="Spotify"
+                width={16}
+                height={16}
+                className="saturate-0"
+              />
+              <p>
+                on{" "}
+                <span className="font-semibold text-opacity-90">Spotify</span>
+              </p>
+            </div>
+          </div>
+        </motion.a>
+      ) : (
+        <motion.a
+          target="_blank"
+          href="https://open.spotify.com/user/ock5719fh26056w67awzv7rty"
+          initial={{ opacity: 0, y: 10 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.4, delay: 0.5 }}
+          className="flex w-72 select-none items-center justify-start gap-2 overflow-hidden rounded-lg border-2 border-white border-opacity-5 bg-white bg-opacity-5 p-3 2xl:w-80 2xl:gap-3 3xl:mt-6"
+        >
+          <Image
+            src="/icons/spotify.svg"
+            alt="Spotify"
+            width={20}
+            height={20}
+            className="saturate-0"
+          />
+          <p className="2xl:text-lg 3xl:text-xl">currently not listening...</p>
+        </motion.a>
+      )}
+    </Fragment>
+  );
+});
+
+export default Presence;
