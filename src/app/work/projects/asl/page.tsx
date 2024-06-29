8607398437fb34a22fad5d3437a006ac6c4a8ef5
Kevin Thomas <kevin.jt2007@gmail.com>
2024-06-29 14:44:47 -0700

    feat: added asl project

---
+++ src/app/work/projects/asl/page.tsx
@@ -0,0 +1,9 @@
+import Project from "../components/Project";
+
+export default function BCYDC() {
+  return (
+    <Project name="Sign Language Translation" image="asl.png" banner="asl.png">
+      <p>hi</p>
+    </Project>
+  );
+}
