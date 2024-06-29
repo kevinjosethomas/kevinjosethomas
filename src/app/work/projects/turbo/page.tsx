35d3a4a94c24d316a05dbeaff5633f005cc1f7d5
Kevin Thomas <kevin.jt2007@gmail.com>
2024-06-29 14:48:00 -0700

    feat: added turbo page

---
+++ src/app/work/projects/turbo/page.tsx
@@ -0,0 +1,9 @@
+import Project from "../components/Project";
+
+export default function Turbo() {
+  return (
+    <Project name="Turbo" image="turbo.png" banner="turbo.svg">
+      <p>hi</p>
+    </Project>
+  );
+}
