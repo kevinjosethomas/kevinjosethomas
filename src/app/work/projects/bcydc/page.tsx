19477e3d01bc24278e45318b7e37da3a8cbcd04a
Kevin Thomas <kevin.jt2007@gmail.com>
2024-06-29 14:33:40 -0700

    feat: added bcydc page

---
+++ src/app/work/projects/bcydc/page.tsx
@@ -0,0 +1,13 @@
+import Project from "../components/Project";
+
+export default function BCYDC() {
+  return (
+    <Project
+      name="British Columbia Youth Developer Collective"
+      image="bcydc.png"
+      banner="bcydc.png"
+    >
+      <p>hi</p>
+    </Project>
+  );
+}
