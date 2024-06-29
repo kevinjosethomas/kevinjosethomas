5429e6b90cbcbfb02f2a2ab3c001eb1f2b293e0c
Kevin Thomas <kevin.jt2007@gmail.com>
2024-06-29 14:38:16 -0700

    feat: added minecraft.global page

---
+++ src/app/work/projects/minecraft/page.tsx
@@ -0,0 +1,13 @@
+import Project from "../components/Project";
+
+export default function Minecraft() {
+  return (
+    <Project
+      name="minecraft.global"
+      image="minecraft.global.png"
+      banner="minecraft.global.png"
+    >
+      <p>hi</p>
+    </Project>
+  );
+}
