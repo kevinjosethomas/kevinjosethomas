6be10d2a7ba412f8752d9bb917cb6a45fa9fef3d
Kevin Thomas <kevin.jt2007@gmail.com>
2024-06-29 14:30:44 -0700

    feat: added more content

--- src/app/work/projects/components/Project.tsx
+++ src/app/work/projects/components/Project.tsx
@@ -1,17 +1,36 @@
+import Link from "next/link";
 import Image from "next/image";
 
 export default function Project({
   name,
   image,
+  banner,
   children,
 }: {
   name: string;
   image: string;
+  banner: string;
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex flex-col">
-      <div className="flex items-center gap-3">
+    <div className="flex flex-col gap-4">
+      <Link
+        href="/work"
+        className="flex h-10 w-10 items-center justify-center rounded-full bg-neutral-800 transition duration-300 hover:bg-neutral-700"
+      >
+        <Image src="/icons/left-arrow.svg" width={16} height={16} />
+      </Link>
+      <div className="relative h-[300px] w-[1024px] select-none overflow-hidden">
+        <div className="absolute h-full w-full bg-gradient-to-b from-transparent to-black"></div>
+        <Image
+          draggable={false}
+          src={`/images/banners/${banner}`}
+          alt={name}
+          width={1024}
+          height={300}
+        />
+      </div>
+      <div className="z-50 -mt-24 flex items-center gap-3">
         <Image
           src={`/images/projects/${image}`}
           alt={name}
@@ -20,6 +39,7 @@ export default function Project({
         />
         <p className="text-4xl font-bold text-white">{name}</p>
       </div>
+      <div className="flex flex-col">{children}</div>
     </div>
   );
 }
--- src/app/work/projects/disbots/page.tsx
+++ src/app/work/projects/disbots/page.tsx
@@ -2,8 +2,16 @@ import Project from "../components/Project";
 
 export default function Disbots() {
   return (
-    <Project name="disbots.gg" image="disbots.png">
-      <p></p>
+    <Project name="disbots.gg" image="disbots.png" banner="disbots.png">
+      <iframe
+        width="1024"
+        height="576"
+        src="https://www.youtube.com/embed/XCjmf23Ef0w?si=wV-RSw1sGmJM6acg"
+        title="disbots.gg Trailer"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
     </Project>
   );
 }
