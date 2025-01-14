0958260a6ee17d8221b2a412e7ac76eb0948ee4f
Kevin Thomas <kevin.jt2007@gmail.com>
2025-01-13 19:36:14 -0800

    chore: add redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/signpaper",
+        destination: "https://arxiv.org/abs/2408.09311",
+        permanent: true,
+      },
+      {
+        source: "/signvideo",
+        destination: "https://youtu.be/uuPxMWQRoXc?si=XsrEzM87X40I0XCj",
+        permanent: true,
+      },
+      {
+        source: "/signgithub",
+        destination:
+          "https://github.com/kevinjosethomas/sign-language-processing",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
